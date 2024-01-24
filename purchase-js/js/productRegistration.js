7dbb9db05aa9862279b3ba71324248196280d5c9
nick19y <godoi.nic.nic@gmail.com>
2024-01-24 17:36:16 -0300

    desenvolvimento do localstorage de imagem

--- purchase-js/js/productRegistration.js
+++ purchase-js/js/productRegistration.js
@@ -4,36 +4,58 @@ const inputPrice = document.querySelector(".product-price");
 const products = JSON.parse(localStorage.getItem('products')) || [];
 
 
-formAddProduct.addEventListener("submit", (evento)=>{
-    evento.preventDefault();
-    const product={
-        name: inputName.value,
-        price: inputPrice.value
-    }
-    products.push(product);
-    localStorage.setItem('products', JSON.stringify(products));
-    location.reload();
-})
-
 let profilePic = document.getElementById("profile-pic");
 let inputFile = document.getElementById("input-file");
 
-inputFile.addEventListener("change", function(){
-    const reader = new FileReader();
 
-    console.log(this.files);
-    reader.addEventListener("load", ()=>{
+function addItemImage(){
+    inputFile.addEventListener("change", function(){
+        const reader = new FileReader();
+        // leitor de arquivo
+        console.log(this.files[0]);
+        reader.addEventListener("load", ()=>{
+            localStorage.setItem("recent-image", reader.result);
+            // resultado do leitor de arquivo atribuído como uma strig ao localStorage
+        });
+        reader.readAsDataURL(this.files[0])
+        // leitor de arquivo "lendo" o arquivo como uma URL
         console.log(reader.result);
-        localStorage.setItem("recent-image", reader.result);
+        return reader.result;
     })
+}
 
-    reader.readAsDataURL(this.files[0])
-})
+
+// usar esse código na hora de exibir a imagem
 document.addEventListener("DOMContentLoaded", ()=>{
     const recentImageDataURL = localStorage.getItem("recent-image");
     if(recentImageDataURL){
         profilePic.setAttribute("src", recentImageDataURL);
     }
-    console.log("DOM fully loaded and parsed");
-    
 })
+let image = addItemImage();
+console.log(image);
+
+formAddProduct.addEventListener("submit", (evento)=>{
+    evento.preventDefault();
+
+
+    const product={
+        name: inputName.value,
+        price: inputPrice.value,
+        img: image,
+    }
+    products.push(product);
+    localStorage.setItem('products', JSON.stringify(products));
+    location.reload();
+
+
+
+    console.log(this.files);
+    // this.files só funciona com o evento change e não o submit
+})
+
+
+
+
+
+
