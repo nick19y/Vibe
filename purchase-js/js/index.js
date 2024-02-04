9dd11578caaef9e56ee329f01ef8773a30e488ce
nick19y <godoi.nic.nic@gmail.com>
2024-02-04 14:41:42 -0300

    desenvolvimento da página de perfil de usuário

--- purchase-js/js/index.js
+++ purchase-js/js/index.js
@@ -1,7 +1,4 @@
 const products = JSON.parse(localStorage.getItem('products')) || [];
-const productsShoppingCart = JSON.parse(localStorage.getItem('productsShoppingCart')) || [];
-
-
 // HTML
 const productsHtml = document.querySelector(".products");
 const btnTeste = document.querySelector(".teste");
@@ -14,39 +11,10 @@ function createProductElement(product){
         <div class="update-btn"></div>
     </div>
     `
-
-    const updateBtnJs = document.createElement('button');
-    const updateBtn = document.querySelector(".update-btn");
 }
 products.forEach(element => {
     createProductElement(element);
 });
-
-
-let productsList = document.querySelector(".item-list-li");
-function createShoppingCartElement(product){
-    productsList.innerHTML+=
-        `
-        <li class="shopping-cart-item">
-                    <img src="${product.img}" alt="" class="img-shopping-cart">
-                    <div class="name-price">
-                        <div class="shopping-name">${product.name}</div>
-                        <div class="shopping-price">R$${product.price}</div>
-                    </div>
-                    <div class="amount-close-btn">
-                        <img src="img/close-btn (2).png" alt="" class="close-btn-item-list">
-                        <div class="amount-btns">
-                            <button class="remove-amount-btn alter-amount-btn">
-                                <div class="remove-amount-btn-content">-</div>
-                            </button>
-                            <h2>1</h2>
-                            <button class="add-amount-btn alter-amount-btn">+</button>
-                        </div>
-                    </div>
-                </li>
-        `
-}
-
 products.map((item)=>{
     let productItem = document.querySelector('.models .products .product').cloneNode(true);
     document.querySelector('.productArea').append(productItem);
@@ -59,16 +27,6 @@ products.map((item)=>{
         localStorage.setItem('productsShoppingCart', JSON.stringify(productsShoppingCart));
     })  
 })
-
-let shoppingCart = document.querySelector('.shopping-cart');
-
-shoppingCart.addEventListener('click', ()=>{
-    productsList.innerHTML="";
-    productsShoppingCart.forEach(product=>{
-        createShoppingCartElement(product);
-    })     
-})
-
 
 let exitBtn = document.querySelector(".exit-btn")
 exitBtn.addEventListener("click", ()=>{
---
+++ purchase-js/js/shopping-cart.js
@@ -0,0 +1,33 @@
+const productsShoppingCart = JSON.parse(localStorage.getItem('productsShoppingCart')) || [];
+
+let productsList = document.querySelector(".item-list-li");
+function createShoppingCartElement(product){
+    productsList.innerHTML+=
+        `
+        <li class="shopping-cart-item">
+                    <img src="${product.img}" alt="" class="img-shopping-cart">
+                    <div class="name-price">
+                        <div class="shopping-name">${product.name}</div>
+                        <div class="shopping-price">R$${product.price}</div>
+                    </div>
+                    <div class="amount-close-btn">
+                        <img src="img/close-btn (2).png" alt="" class="close-btn-item-list">
+                        <div class="amount-btns">
+                            <button class="remove-amount-btn alter-amount-btn">
+                                <div class="remove-amount-btn-content">-</div>
+                            </button>
+                            <h2>1</h2>
+                            <button class="add-amount-btn alter-amount-btn">+</button>
+                        </div>
+                    </div>
+                </li>
+        `
+}
+let shoppingCart = document.querySelector('.shopping-cart');
+
+shoppingCart.addEventListener('click', ()=>{
+    productsList.innerHTML="";
+    productsShoppingCart.forEach(product=>{
+        createShoppingCartElement(product);
+    })     
+})
