3e2d8dba01435de0b60a0b85903f60a1c121ab45
nick19y <godoi.nic.nic@gmail.com>
2024-03-22 23:54:49 -0300

    tratamento de senha errada na aplicação

--- purchase-js/js/index.js
+++ purchase-js/js/index.js
@@ -1,30 +1,39 @@
 const formindex = document.querySelector(".form-index");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("password");
-const btnTeste = document.querySelector(".btn");
+const btn = document.querySelector(".btn");
 const users = JSON.parse(localStorage.getItem("user"));
 
 // usar session storage
 console.log(users)
 users.forEach((user) =>console.log(user))
 function checkUser(){
+    let verify;
     userEmailindex = userEmail.value;
     userPasswordindex = userPassword.value;
     users.forEach((user) =>{
         if(userEmailindex == user.email && userPasswordindex == user.password){
             sessionStorage.setItem("logged-user", JSON.stringify(user));
-            window.open('main.html', '_self');
             console.log(user);
+            verify=true;
+            return verify;
         }
         else{
-            document.write("Senha incorreta");
-            return;
-            // FAZER TRATAMENTO COM ID 
+            verify = false;
+            return verify; 
         }
     })
+    console.log(verify);
+    console.log("fim")
+    if(verify){
+        window.open('main.html', '_self');
+    } else{
+        document.write("Senha incorreta");
+        console.log(verify);
+    }
 }
 
-btnTeste.addEventListener("click", ()=>{
+btn.addEventListener("click", ()=>{
     checkUser();
     // window.open('main.html', '_self');
 })
